3295f808c6148e7dc45d2b1bef4444960722025b
Seungsoo Lee <js815002@gmail.com>
2022-11-22 00:18:52 +0900

    Fix optimization error and make iteration

--- pose_diff/src/main.cpp
+++ pose_diff/src/main.cpp
@@ -39,8 +39,8 @@ int main() {
   print_vector(state_6d);
   std::cout << "\n\n";
 
-  const double KRotStdDev = 0.5;
-  const double kTransStdDev = 0.3;
+  const double KRotStdDev = 0.05;
+  const double kTransStdDev = 0.1;
   std::default_random_engine random_engine(time(nullptr));
   std::normal_distribution<double> rot_gaussian(0, KRotStdDev);
   std::normal_distribution<double> trans_gaussian(0, kTransStdDev);
@@ -61,9 +61,10 @@ int main() {
 
   Eigen::Vector3d rot_vector = state_6d.head(3);
   Eigen::Matrix3d conversion_result =
-      pose_diff::Rotation::AxisAngleToMatrix(rot_vector) - pose_matrix.block<3, 3>(0, 0);
-  std::cout << "Matrix normalize R - R` (R` from axis-angle)\n";
-  std::cout << conversion_result.norm() << "\n\n\n";
+      pose_diff::Rotation::AxisAngleToMatrix(rot_vector).transpose() * pose_matrix.block<3, 3>(0, 0);
+  Eigen::Vector3d conversion_result_vector = pose_diff::Rotation::MatrixToAxisAngle(conversion_result);
+  std::cout << "Vector normalize log(R^TR`) (R` from axis-angle)\n";
+  std::cout << conversion_result_vector.norm() << "\n\n\n";
 
   const int kPointNum = 20;
   const double kPointPosStdDev = 5.0;
@@ -100,13 +101,13 @@ int main() {
   for (int i = 0; i < kPointNum; i++) {
     // e = Rp' + t - p
     error_vector.segment<3>(3 * i) =
-        pose_diff::Transform::Transform3DPoint(transformation_matrix_with_noise_c2_wrt_c1, c2_point_list[0]) -
-        c1_point_list[0];
+        pose_diff::Transform::Transform3DPoint(transformation_matrix_with_noise_c2_wrt_c1, c2_point_list[i]) -
+        c1_point_list[i];
   }
   std::cout << "Error vector\n";
   print_vector(error_vector);
-  std::cout << "Error vector mean\n";
-  std::cout << error_vector.mean();
+  std::cout << "Error vector norm\n";
+  std::cout << error_vector.norm();
   std::cout << "\n\n";
 
   Eigen::MatrixXd jacobian_matrix(3 * kPointNum, 6);
@@ -124,24 +125,41 @@ int main() {
 
   // e(x + dx) (\approx) e + H dx = 0
   // dx = - H.inv e
-  Eigen::VectorXd state_diff = -pose_diff::Matrix::GetPseudoInverseMatrix(jacobian_matrix) * error_vector;
   Eigen::VectorXd current_state = state_diff_with_noise_c2_wrt_c1.Get6DState();
-  Eigen::VectorXd next_state = current_state + state_diff;
-  next_state.head(3) =
-      pose_diff::Rotation::MatrixToAxisAngle(pose_diff::Rotation::AxisAngleToMatrix(current_state.head(3)) *
-                                             pose_diff::Rotation::AxisAngleToMatrix(state_diff.head(3)));
-  Eigen::Matrix4d transformation_matrix = pose_diff::State(next_state).GetTransformationMatrix();
-  for (int i = 0; i < kPointNum; i++) {
-    // e = Rp' + t - p
-    error_vector.segment<3>(3 * i) =
-        pose_diff::Transform::Transform3DPoint(transformation_matrix, c2_point_list[0]) - c1_point_list[0];
+  const double kInitialErrorNorm = error_vector.norm();
+  const int kMaxIter = 100;
+  int iter_count = 0;
+  while (iter_count++ < kMaxIter) {
+    Eigen::VectorXd state_diff = -pose_diff::Matrix::GetPseudoInverseMatrix(jacobian_matrix) * error_vector;
+    Eigen::VectorXd next_state = current_state + state_diff;
+    next_state.head(3) =
+        pose_diff::Rotation::MatrixToAxisAngle(pose_diff::Rotation::AxisAngleToMatrix(state_diff.head(3)) *
+                                               pose_diff::Rotation::AxisAngleToMatrix(current_state.head(3)));
+    Eigen::Matrix4d transformation_matrix = pose_diff::State(next_state).GetTransformationMatrix();
+    for (int i = 0; i < kPointNum; i++) {
+      // e = Rp' + t - p
+      error_vector.segment<3>(3 * i) =
+          pose_diff::Transform::Transform3DPoint(transformation_matrix, c2_point_list[i]) - c1_point_list[i];
+    }
+    std::cout << "Iter " << iter_count << "\n";
+    std::cout << "Error vector\n";
+    print_vector(error_vector);
+    std::cout << "Error vector mean\n";
+    std::cout << error_vector.norm();
+    std::cout << "\n\n";
+
+    double error_norm = error_vector.norm();
+    if (error_norm < kInitialErrorNorm * 1e-7 || error_norm < 1e-6) {
+      std::cout << "Succeeded to optimize. error: " << error_norm << ", initial error:" << kInitialErrorNorm << "\n";
+      break;
+    }
+
+    current_state = next_state;
   }
-  std::cout << "Iter 1\n";
-  std::cout << "Error vector\n";
-  print_vector(error_vector);
-  std::cout << "Error vector mean\n";
-  std::cout << error_vector.mean();
-  std::cout << "\n\n";
+  if (iter_count > kMaxIter) {
+    std::cout << "Failed to optimize. error: " << error_vector.norm() << ", initial error:" << kInitialErrorNorm
+              << "\n";
+  }
 
   return 0;
 }
